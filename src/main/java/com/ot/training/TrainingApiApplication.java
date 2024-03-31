578a4b4dc808284887cafe7ff3d41f4f38e966ad
Keisuke Ota <k.ota109@outlook.com>
2024-04-01 02:10:36 +0900

    create project

---
+++ src/main/java/com/ot/training/TrainingApiApplication.java
@@ -0,0 +1,13 @@
+package com.ot.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ot/training/TrainingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ot.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
